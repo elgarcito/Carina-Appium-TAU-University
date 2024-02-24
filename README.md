c0f9de01ddef47cbd6d6fa6e4967148cd59fae82
elgaricito <edgarjaguirre@hotmail.com>
2024-02-24 16:21:05 -0300

    Adding the test steps and all the test

--- README.md
+++ README.md
@@ -25,3 +25,14 @@ Each branch is going to have
 the name of the class finished (available from chapter 4.1).
 Due to the lack of IOS all the practice will be done in Android.
 Finally, in the main branch you will find all exercises together
+
+### Chapter 5.2 - Mobile Gestures - Scroll Down
+In this exercise, we are going to practise how to scroll down
+To achieve this we are going to use the IAndroidUtils of carina framework
+.The principal method is `swipe()`
+This test in MobileTest.java called `scroll_test()` does te following:
+1. Open the app
+2. Click the view Option
+3. Scroll down 
+4. Check if the List button is present and compare it with the string "Lists"
+5. If everything is ok the test pass otherwise it fails
--- src/test/java/com/solvd/carina/example/MobileTests.java
+++ src/test/java/com/solvd/carina/example/MobileTests.java
@@ -1,9 +1,11 @@
 package com.solvd.carina.example;
 import com.solvd.carina.example.gui.pages.common.AppHomePageBase;
 import com.solvd.carina.example.gui.pages.common.AppPageBase;
+import com.solvd.carina.example.gui.pages.common.ViewPageBase;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
 import com.zebrunner.carina.utils.android.IAndroidUtils;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MobileTests implements IAbstractTest, IAndroidUtils {
@@ -13,5 +15,13 @@ public class MobileTests implements IAbstractTest, IAndroidUtils {
         AppHomePageBase appHomePage =initPage(getDriver(), AppHomePageBase.class);
         AppPageBase appPageBase= appHomePage.clickAppPageBase();
         appPageBase.getMenuList();
+    }
+
+    @Test(description = "scroll test")
+    public void scroll_test(){
+        AppHomePageBase homePage=initPage(getDriver(), AppHomePageBase.class);
+        ViewPageBase viewPage= homePage.clickViewPage();
+        viewPage.scrollDownToObject();
+        Assert.assertEquals(viewPage.getListOptionText(),"Lists","Wrong message");
     }
 }
